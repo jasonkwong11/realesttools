32fbc23846704d9dd9eead98aa59a15f790bb71c
Jason <jasonkwong11@gmail.com>
2016-09-23 16:00:38 -0400

    finished assigning principal property's data from GetDeepComps API, initial work on comparables hash

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -23,7 +23,28 @@ class SearchesController < ApplicationController
   
   comps = Rillow.new('X1-ZWz1fgbe5szxmz_29gz1')
   response = comps.get_deep_comps(principle_property.zpid, 25) 
-  
+
+  principle_property.percentile = response["response"][0]["properties"][0]["principal"][0]["zestimate"][0]["percentile"][0]
+  principle_property.year_tax_assessed = response["response"][0]["properties"][0]["principal"][0]["taxAssessmentYear"][0]
+  principle_property.tax_assessment = response["response"][0]["properties"][0]["principal"][0]["taxAssessment"][0]
+  principle_property.year_built = response["response"][0]["properties"][0]["principal"][0]["yearBuilt"][0]
+  principle_property.square_feet = response["response"][0]["properties"][0]["principal"][0]["lotSizeSqFt"][0]
+  principle_property.bathrooms = response["response"][0]["properties"][0]["principal"][0]["bathrooms"][0]
+  principle_property.bedrooms = response["response"][0]["properties"][0]["principal"][0]["bedrooms"][0]
+  principle_property.last_sold_date = response["response"][0]["properties"][0]["principal"][0]["lastSoldDate"][0]
+  principle_property.last_sold_price = response["response"][0]["properties"][0]["principal"][0]["lastSoldPrice"][0]["content"]
+
+  #principle_property object is has all of it's associated data
+  #now iterate through the comparables has to instantiate new comp objects
+  comparables_hash = response["response"][0]["properties"][0]["comparables"][0]["comp"]
+
+  comparables_hash.each do ||
+
+  end
+
+  require 'pry'
+  binding.pry
+
   render 'search'
   end
 end
