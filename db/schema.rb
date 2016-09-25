dcf3c5dd7ff182eb6e2d8fe26111bd8418e7b88c
Jason <jasonkwong11@gmail.com>
2016-09-24 23:35:42 -0400

    switched sqlite3 database for postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160923204741) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comps", force: :cascade do |t|
     t.string  "street"
     t.string  "city"
